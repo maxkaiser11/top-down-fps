cf4b6e54033d0ffd55b11560cf090d07dfeee5c1
crubz11 <max@kaiser.family>
2024-10-19 17:15:12 +0200

    removing bullets once they are out of bounds

--- main.lua
+++ main.lua
@@ -52,9 +52,18 @@ function love.update(dt)
 		end
 	end
 
+	-- Bullet direction
 	for i, b in ipairs(bullets) do
 		b.x = b.x + (math.cos(b.direction) * b.speed * dt)
 		b.y = b.y + (math.sin(b.direction) * b.speed * dt)
+	end
+
+	-- Removing bullet once it exits the screen
+	for i = #bullets, 1, -1 do
+		local b = bullets[i]
+		if b.x < 0 or b.y < 0 or b.x > love.graphics.getWidth() or b.y > love.graphics.getHeight() then
+			table.remove(bullets, i)
+		end
 	end
 end
 
